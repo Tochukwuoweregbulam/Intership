38ff0c778c6adb80dd25c9f035ffc3f6f864043c
Tochukwuoweregbulam <oweregbulamt@gmail.com>
2026-07-18 13:54:25 +0100

    arrange of my weekly task

---
+++ Ankur/week 5/variables.ts
@@ -0,0 +1,15 @@
+
+let name: string = "Joseph";
+
+let age: number = 25;
+
+
+let isStudent: boolean = true;
+
+
+
+
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
